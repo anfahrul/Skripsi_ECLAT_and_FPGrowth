6d6f2e8343e419d200a93b731e0a92f3d6903f3c
anfahrul <an.fahrulan@gmail.com>
2023-11-05 11:35:44 +0800

    updated interface of dashboard

--- src/controllers/dashboardController.py
+++ src/controllers/dashboardController.py
@@ -4,16 +4,19 @@ from src.models.product import Product
 from src.models.transaction import Transaction
 from src.models.user import User
 from src.models.mining import MiningProcess
+from flask_login import current_user
 
 
 def dashboardIndex():
     products = Product.query.all()
     transactions = Transaction.query.all()
+    currentUser = User.query.filter_by(email=current_user.email).first()
     users = User.query.all()
     miningProcess = MiningProcess.query.all()
     
     return render_template('dashboard.html',
                            products=products,
                            transactions=transactions,
+                           currentUser=currentUser,
                            users=users,
                            miningProcess=miningProcess)
