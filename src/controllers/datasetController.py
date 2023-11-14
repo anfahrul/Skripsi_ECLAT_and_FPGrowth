601b0a271f9cc21ab7c660344ef90d968da08a37
anfahrul <an.fahrulan@gmail.com>
2023-11-14 10:53:39 +0800

    create error handler when importing the sales data

--- src/controllers/datasetController.py
+++ src/controllers/datasetController.py
@@ -12,85 +12,92 @@ def importIndex():
     
 
 def doImportFile():
-    if 'file' not in request.files:
-        return "No file part"
-    
-    file = request.files['file']
-    
-    if file.filename == '':
-        return "No selected file"
-    
-    if file:
-        # Membaca file Excel menggunakan pandas
-        df = pd.read_excel(file)
+    try:
+        if 'file' not in request.files:
+            return "No file part"
+        
+        file = request.files['file']
+        
+        if file.filename == '':
+            return "No selected file"
+        
+        if file:
+            # Membaca file Excel menggunakan pandas
+            df = pd.read_excel(file)
 
-        for index, row in df.iterrows():
-            date = row['TANGGAL']
-            transaction_id = str(row['NO. FAKTUR'])
-            item_code = str(row['KODE BARANG'])
-            name = row['NAMA BARANG']
-            unit = row['SATUAN']
-            quantity = row['QTY']
-            price = row['HARGA']
-            # subtotal = row['SUBTOTAL']
+            for index, row in df.iterrows():
+                date = row['TANGGAL']
+                transaction_id = str(row['NO. FAKTUR'])
+                item_code = str(row['KODE BARANG'])
+                name = row['NAMA BARANG']
+                unit = row['SATUAN']
+                quantity = row['QTY']
+                price = row['HARGA']
+                # subtotal = row['SUBTOTAL']
 
-            # Menyimpan data produk ke tabel 'product'
-            existing_product = Product.query.filter_by(itemCode=item_code).first()
-            if existing_product:
-                existing_product.name = name
-                existing_product.unit = unit
-                existing_product.price = price
-            else:
-                product = Product(itemCode=item_code, name=name, unit=unit, price=price)
-                db.session.add(product)
+                # Menyimpan data produk ke tabel 'product'
+                existing_product = Product.query.filter_by(itemCode=item_code).first()
+                if existing_product:
+                    existing_product.name = name
+                    existing_product.unit = unit
+                    existing_product.price = price
+                else:
+                    product = Product(itemCode=item_code, name=name, unit=unit, price=price)
+                    db.session.add(product)
 
-            # Menyimpan data transaksi ke tabel 'transaction'
-            existing_transaction = Transaction.query.filter_by(transaction_id=transaction_id).first()
-            if not existing_transaction:
-                transaction = Transaction(transaction_id=transaction_id, date=date, total_price=0)  # Total harga akan dihitung nanti
-                db.session.add(transaction)
-                existing_transaction = transaction
-            else:
-                existing_transaction.date = date
+                # Menyimpan data transaksi ke tabel 'transaction'
+                existing_transaction = Transaction.query.filter_by(transaction_id=transaction_id).first()
+                if not existing_transaction:
+                    transaction = Transaction(transaction_id=transaction_id, date=date, total_price=0)  # Total harga akan dihitung nanti
+                    db.session.add(transaction)
+                    existing_transaction = transaction
+                else:
+                    existing_transaction.date = date
+                
+                # Menyimpan data produk transaksi ke tabel 'transaction_products'
+                existing_transaction_product = TransactionProduct.query.filter_by(transaction_id=transaction_id, itemCode=item_code).first()
+                if not existing_transaction_product:
+                    # Jika belum ada, tambahkan data baru ke tabel 'transaction_products'
+                    transaction_product = TransactionProduct(transaction_id=transaction_id, itemCode=item_code, quantity=quantity)
+                    db.session.add(transaction_product)
+                    existing_transaction_product = transaction_product
+                
+                    # Update total price  
+                    # subtotal_for_product = price * quantity
+                    # existing_transaction.total_price += subtotal
+                else:
+                    existing_transaction_product.quantity = quantity
             
-            # Menyimpan data produk transaksi ke tabel 'transaction_products'
-            existing_transaction_product = TransactionProduct.query.filter_by(transaction_id=transaction_id, itemCode=item_code).first()
-            if not existing_transaction_product:
-                # Jika belum ada, tambahkan data baru ke tabel 'transaction_products'
-                transaction_product = TransactionProduct(transaction_id=transaction_id, itemCode=item_code, quantity=quantity)
-                db.session.add(transaction_product)
-                existing_transaction_product = transaction_product
-              
-                # Update total price  
-                # subtotal_for_product = price * quantity
-                # existing_transaction.total_price += subtotal
-            else:
-                existing_transaction_product.quantity = quantity
-        
-        
-        # Query untuk menghitung total_price dari tabel transaction_products
-        subtotal_query = db.session.query(
-            TransactionProduct.transaction_id,
-            func.sum(TransactionProduct.quantity * Product.price).label('subtotal')
-        ).join(
-            Product,
-            TransactionProduct.itemCode == Product.itemCode
-        ).group_by(TransactionProduct.transaction_id).subquery()
+            
+            # Query untuk menghitung total_price dari tabel transaction_products
+            subtotal_query = db.session.query(
+                TransactionProduct.transaction_id,
+                func.sum(TransactionProduct.quantity * Product.price).label('subtotal')
+            ).join(
+                Product,
+                TransactionProduct.itemCode == Product.itemCode
+            ).group_by(TransactionProduct.transaction_id).subquery()
 
-        # Update total_price di tabel transactions
-        db.session.query(Transaction).filter(
-            Transaction.transaction_id == subtotal_query.c.transaction_id
-        ).update(
-            {Transaction.total_price: subtotal_query.c.subtotal},
-            synchronize_session=False
-        )
-         
-        db.session.commit()
+            # Update total_price di tabel transactions
+            db.session.query(Transaction).filter(
+                Transaction.transaction_id == subtotal_query.c.transaction_id
+            ).update(
+                {Transaction.total_price: subtotal_query.c.subtotal},
+                synchronize_session=False
+            )
+            
+            db.session.commit()
+            
+            
+            dataframe_html = df.to_html(classes='table table-bordered', index=False, escape=False,
+                                        render_links=True, 
+                                        formatters={"header": lambda x: f'<th style="text-align: center;">{x}</th>'})
         
+        flash('Data transaksi penjualan berhasil diimport.')
         
-        dataframe_html = df.to_html(classes='table table-bordered', index=False, escape=False,
-                                    render_links=True, 
-                                    formatters={"header": lambda x: f'<th style="text-align: center;">{x}</th>'})
-    
-    flash('Data transaksi penjualan berhasil diimport.')
+    except Exception as e:
+        # flash(f'Error: {str(e)}')
+        flash(f'File tidak sesuai ketentuan, periksa kembali format file dan nama kolom.')
+        dataframe_html = None
+        
     return render_template('dataset/import.html', dataframe_html=dataframe_html)
